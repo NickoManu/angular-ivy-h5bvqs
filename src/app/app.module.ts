f8809faf7e83a5023310bb5df5cca8835fda53fa
NickoManu <manuel.nicoletta@gmail.com>
2020-12-03 11:25:53 +0100

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,12 @@ import { UsersComponent } from "./users/users.component";
 import { UserComponent } from './user/user.component';
 import { UserService } from "./users/user.service";
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, UsersComponent, UserComponent, UserDetailComponent],
+  declarations: [AppComponent, HelloComponent, UsersComponent, UserComponent, UserDetailComponent, NavComponent],
   bootstrap: [AppComponent],
   providers: [UserService]
 })
